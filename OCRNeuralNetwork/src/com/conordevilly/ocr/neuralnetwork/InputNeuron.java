d8f9ef3430825b9051b0b3b25b62f4f38e243bda
Conor Devilly <c14311816@mydit.ie>
2016-03-23 18:06:19 +0000

    Better Neuron super class

--- OCRNeuralNetwork/src/com/conordevilly/ocr/neuralnetwork/InputNeuron.java
+++ OCRNeuralNetwork/src/com/conordevilly/ocr/neuralnetwork/InputNeuron.java
@@ -1,31 +1,17 @@
 package com.conordevilly.ocr.neuralnetwork;
 
 /*
- * Basic Neuron.
- * Recieves a single pixel as input.
- * Simply forwards this to all output neurons
+ * Simple perceptron.
+ * Takes a single integer input and forwards it another layer
  */
 public class InputNeuron extends Neuron{
 	private static final long serialVersionUID = 5294755918675102781L;
-	int input;
-	int index;
-	OutputNeuron[] outputs;
-	
-	public InputNeuron(int in, int ind, OutputNeuron[] out){
-		input = in;
-		index = ind;
-		outputs = out;
+
+	public InputNeuron() {
+		super(1);
+		weights[0] = 1;
 	}
-	
+
 	@Override
-	public void forward(){
-		for(int i = 0; i < outputs.length; i++){
-			outputs[i].inputs[index] = (int) process();
-		}
-	}
-	
-	@Override
-	public float process(){
-		return input;
-	}
+	public void forward(){};
 }
--- OCRNeuralNetwork/src/com/conordevilly/ocr/neuralnetwork/Neuron.java
+++ OCRNeuralNetwork/src/com/conordevilly/ocr/neuralnetwork/Neuron.java
@@ -6,23 +6,39 @@ public abstract class Neuron implements java.io.Serializable{
 	float[] weights;
 	float output;
 	
+	public Neuron(int numInputs){
+		inputs = new float[numInputs];
+		weights = new float[numInputs];
+	}
+	
+	//Set the inputs
+	public void giveInputs(float[] in){
+		chkArraySize(in, inputs);
+		this.inputs = in;
+	}
+	
 	//Basic template for processing inputs
 	public float process(){
 		float ret = 0;
-		
 		for(int i = 0; i < inputs.length; i++){
 			ret += inputs[i] * weights[i];
 		}
-		
 		return ret;
 	}
 
 	//General rule for correcting weights
 	public void correctWeights(float[] corrections){
+		chkArraySize(corrections, weights);
 		for(int i = 0; i < corrections.length; i++){
 			weights[i] += corrections[i];
 		}
 	}
 	
+	//If two arrays are not of the same size, throw an exception
+	private void chkArraySize(float[] arr1, float[] arr2){
+		if(arr1.length != arr2.length) throw new ArrayIndexOutOfBoundsException();
+	}
+	
+	//Forward output to the next layer
 	public abstract void forward();
 }
