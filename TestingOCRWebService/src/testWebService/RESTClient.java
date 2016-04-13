67b93c675338ef3a7887922784c11845d71526ca
Conor Devilly <c14311816@mydit.ie>
2016-04-13 19:36:16 +0100

    Client sending image

--- TestingOCRWebService/src/testWebService/RESTClient.java
+++ TestingOCRWebService/src/testWebService/RESTClient.java
@@ -1,54 +1,66 @@
 package testWebService;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
- 
+import java.nio.file.Files;
+import java.util.ArrayList;
+
 import org.json.JSONObject;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.core.util.Base64;
  
 public class RESTClient {
 	public static void main(String[] args) {
-		String str= "";
-		try {
-			InputStream fileIn = new FileInputStream("src/test.json");
-			InputStreamReader reader = new InputStreamReader(fileIn);
-			BufferedReader br = new BufferedReader(reader);
-			String line;
+		RESTClient cli = new RESTClient();
+		cli.sendJSONFile(new File("src/Test/test.png"));
+	}
+	
+	private void sendJSONFile(File in){
+		JSONObject json = new JSONObject();
+		
+		try{
+			String fileData = convFileToString(in);
+			json.put("fileData", fileData);
 
-			while ((line = br.readLine()) != null) {
-				str += line + "\n";
+			URL url = new URL("http://localhost:8080/TestingOCRWebService/api/ocr");
+			URLConnection connection = url.openConnection();
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setConnectTimeout(5000);
+			connection.setReadTimeout(5000);
+
+			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+			out.write(json.toString());
+			out.close();
+
+			BufferedReader response = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line;
+			while ((line = response.readLine()) != null) {
+				System.out.println(line);
 			}
- 
-			JSONObject jsonObject = new JSONObject(str);
-			System.out.println(jsonObject);
- 
-			try {
-				URL url = new URL("http://localhost:8080/TestingOCRWebService/api/ocr");
-				URLConnection connection = url.openConnection();
-				connection.setDoOutput(true);
-				connection.setRequestProperty("Content-Type", "application/json");
-				connection.setConnectTimeout(5000);
-				connection.setReadTimeout(5000);
-				OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
-				out.write(jsonObject.toString());
-				out.close();
- 
-				BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
- 
-				while (in.readLine() != null) {
-				}
-				in.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
- 
-			br.close();
-		} catch (Exception e) {
+			response.close();
+		}catch(Exception e){
 			e.printStackTrace();
 		}
+	}
+	
+	//Converts a file to a Base64 encoded string
+	private String convFileToString(File in) throws IOException{
+		byte[] bytes = Files.readAllBytes(in.toPath());
+		String ret = new String(Base64.encode(bytes));
+		return ret;
+
 	}
 }
--- TestingOCRWebService/src/testWebService/RestService.java
+++ TestingOCRWebService/src/testWebService/RestService.java
@@ -1,24 +1,47 @@
 package testWebService;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
- 
+
+import javax.servlet.ServletContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import com.conordevilly.ocr.neuralnetwork.*;
  
 @Path("/")
 public class RestService {
+	@javax.ws.rs.core.Context 
+	ServletContext context;
+	NeuralNetwork nn;
+	
+	public RestService(){
+		/*
+		NeuralNetwork nn = PersistanceManager.readNN(new File("/nn.data"));
+		if(nn == null){
+			nn = new NeuralNetwork(10, 26);
+		}
+		*/
+	}
+
 	@POST
 	@Path("/ocr")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response processData(InputStream incomingData) {
 		StringBuilder strBuilder = new StringBuilder();
+
+		if(nn == null){
+			//NeuralNetwork nn = PersistanceManager.readNN(new File(context.getContextPath()));
+			System.out.println(context.toString());
+		}
 
 		try {
 			BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
