2e5afe3c4efc9f62cb87a8c194aaa4d07f781826
Conor Devilly <c14311816@mydit.ie>
2016-04-20 14:14:10 +0100

    Comment & clean up android code

--- OCRCamera/src/com/conordevilly/ocr/ocrcamera/OCRRESTClient.java
+++ OCRCamera/src/com/conordevilly/ocr/ocrcamera/OCRRESTClient.java
@@ -1,24 +1,13 @@
 package com.conordevilly.ocr.ocrcamera;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-/*
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-*/
 import java.net.URL;
 import java.net.URLConnection;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.util.Base64;
 import android.util.Log;
 
 public class OCRRESTClient{
@@ -28,43 +17,37 @@ public class OCRRESTClient{
 		this.address = address;
 	}
 	
+	//Sends a string to the service
 	public String send(String toSend){
-		String ret = "";
-		URLConnection connection = null;
-		JSONObject json = null;
+		String ret = null;
+
 		try {
-			json = new JSONObject();
+			//Put the data into a JSON object
+			JSONObject json = new JSONObject();
 			json.put("fileData", toSend);
-			Log.i("OCR", json.toString());
+
+			//Make a connection
 			URL url = new URL(address + "/api/ocr");
-			connection = url.openConnection();
+			URLConnection connection = url.openConnection();
 			connection.setDoOutput(true);
 			connection.setRequestProperty("Content-Type", "application/json");
 			connection.setConnectTimeout(5000);
 			connection.setReadTimeout(5000);
+
+			//Send the data
 			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
 			out.write(json.toString());
 			out.close();
-		}catch(IOException e){
-			Log.e("OCR IOException", e.getMessage());
-		} catch (JSONException e) {
-			Log.e("OCR JSONException", e.getMessage());
-		}
 
-		BufferedReader in = null;
-		try{
-			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		}catch(IOException e){
-			Log.e("OCR IOException", "Getting response");
-		}
-		try{
+			//Read the response
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			String line;
 			while((line = in.readLine()) != null){
 				ret += line;
 			}
 			in.close();
-		} catch(IOException e) {
-			Log.e("OCR: IOException", "Reading Response"); 
+		} catch(Exception e) {
+			Log.e("OCR", e.getMessage());
 		}
 
 		return ret;
--- OCRCamera/src/com/conordevilly/ocr/ocrcamera/RestTask.java
+++ OCRCamera/src/com/conordevilly/ocr/ocrcamera/RestTask.java
@@ -1,14 +1,15 @@
 package com.conordevilly.ocr.ocrcamera;
 
-import java.io.File;
-
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
+/*
+ * Task to connect to the internet to send the data
+ * Android does not allow network activity on the main task so we need to send data via another task
+ */
 public class RestTask extends AsyncTask<String, Void, String>{
 	String url;
 	Context context;
@@ -18,15 +19,19 @@ public class RestTask extends AsyncTask<String, Void, String>{
 		this.context = context;
 	}
 	
+	//Execute the task
 	@Override
 	protected String doInBackground(String... params) {
+		//Create an instance of the client & send the data
 		OCRRESTClient cli = new OCRRESTClient(url);
 		String toSend = params[0];
 		String guess = cli.send(toSend);
+
 		Log.i("OCR Guess:", guess);
 		return guess;
 	}
 	
+	//Display the guess once the task has been completed
 	protected void onPostExecute(String result){
 		super.onPostExecute(result);
 		Builder alertBuilder = new AlertDialog.Builder(context);
